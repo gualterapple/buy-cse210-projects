1cd8f103879f9d7bfb2dc11e8dea7e8574416d24
Gualter dos Santos <franciscobusiness30@gmail.com>
2025-10-06 17:46:55 +0100

    Inheritance activity done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Gualter Santos", "History");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Gualter Santos", "Fractions", "7.3", "8–19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Gualter Santos", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
